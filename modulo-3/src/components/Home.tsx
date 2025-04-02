62dd1ea81bf0e21fa65ae0e57ab4604416116c1e
Felipe <felipe.pita@parclick.com>
2025-04-02 19:47:21 +0200

    Modulo-3: Improvement img

---
+++ modulo-3/src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as styles from "./Home.module.scss";
+import logo1 from "../content/logo_1.png";
+import logo2 from "../content/logo_2.png";
+
+export const Home: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello world from React!</h1>
+      <div className={styles.imagesContainer}>
+        <img className={styles.logo} src={logo1} alt="logo_1" />
+        <img className={styles.logo} src={logo2} alt="logo_2" />
+      </div>
+    </div>
+  );
+}
--- modulo-3/src/main.tsx
+++ modulo-3/src/main.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { StrictMode } from 'react';
 import { createRoot } from "react-dom/client";
+import { Home } from './components/Home';
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <h1>Hello world from React</h1>
-  </div>
-);
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <div>
+//     <h1>Hello world from React</h1>
+//   </div>
+// );
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Home />
+  </StrictMode>,
+)
+
